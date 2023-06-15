81d79849bd7231dfec4d8272a21eb9535f14a719
Alex Sheiko <kelamir@protonmail.com>
2023-06-15 11:37:36 +0300

    feat: save tasks in localStorage

--- src/utils/state.tsx
+++ src/utils/state.tsx
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { persist, devtools } from "zustand/middleware";
 
 interface stateBox {
   tasksCount: number;
@@ -9,12 +10,21 @@ interface stateBox {
   addTask: (task: string) => void;
 }
 
-export const useStateBox = create<stateBox>()((set) => ({
-  tasksCount: 0,
-  increaseCount: () => set((state) => ({ tasksCount: state.tasksCount + 1 })),
-  resetCount: () => set({ tasksCount: 0 }),
+export const useStateBox = create<stateBox>()(
+  persist(
+    devtools((set) => ({
+      tasksCount: 0,
+      increaseCount: () =>
+        set((state) => ({ tasksCount: state.tasksCount + 1 })),
+      resetCount: () => set({ tasksCount: 0 }),
 
-  tasksList: [],
-  addTask: (task) =>
-    set((state) => ({ tasksList: [...state.tasksList, task] })),
-}));
+      tasksList: [],
+      addTask: (task) =>
+        set((state) => ({ tasksList: [...state.tasksList, task] })),
+    })),
+    {
+      name: "Todonner stateBox",
+      getStorage: () => localStorage,
+    }
+  )
+);
