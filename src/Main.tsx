9fe125c60d49120999e3e0f84d24d697db7198e8
Alex Sheiko <kelamir@protonmail.com>
2023-06-15 10:20:14 +0300

    feat: basic CSS

--- src/Main.tsx
+++ src/Main.tsx
@@ -2,9 +2,18 @@ import "./App.css";
 
 function App() {
   return (
-    <>
+    <div className="container">
       <h1>Todonner</h1>
-    </>
+      <button>Add todo</button>
+      <ul className="todo-list">
+        <li className="todo-item">
+          Task 1 <button>Delete</button>
+        </li>
+        <li className="todo-item">
+          Task 2 <button>Delete</button>
+        </li>
+      </ul>
+    </div>
   );
 }
 
