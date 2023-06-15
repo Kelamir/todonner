829cfca9a2b89aa081a670efb6e4a486bc98d961
Alex Sheiko <kelamir@protonmail.com>
2023-06-15 10:46:33 +0300

    feat: states for the app

---
+++ src/utils/state.tsx
@@ -0,0 +1,20 @@
+import { create } from "zustand";
+
+interface stateBox {
+  tasksCount: number,
+  increaseCount: (by: number) => void;
+  resetCount: (by: 0) => void;
+
+  tasksList: string[];
+  addTask: (task: string) => void;
+}
+
+export const useStateBox = create<stateBox>()((set) => ({
+  tasksCount: 0,
+  increaseCount: () =>
+    set((state) => ({ tasksCount: state.tasksCount + 1 })),
+  resetCount: () => set({ tasksCount: 0 }),
+
+  tasksList: [],
+  addTask: (task) => set((state) => ({tasksList: [...state.tasksList, task]}))
+}));
